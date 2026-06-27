9dbef7ec723f6aa0e2b64c69188d10b6c0682045
thesuperRL <ryanli1366@gmail.com>
2026-06-27 16:49:24 -0400

    hosting: export items like dalmatian

--- flake.nix
+++ flake.nix
@@ -1,9 +1,32 @@
 {
   description = "CMUGPT Surface";
 
+  nixConfig = {
+    extra-substituters = [ "https://scottylabs.cachix.org" ];
+    extra-trusted-public-keys = [
+      "scottylabs.cachix.org-1:hajjEX5SLi/Y7yYloiXTt2IOr3towcTGhjbcrFRYcgCw9hcO4=";
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }: { };
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          packages = pkgs.callPackage ./nix/packages.nix { };
+        in
+        {
+          inherit (packages) api web;
+          default = packages.api;
+        }
+      );
+    };
 }
