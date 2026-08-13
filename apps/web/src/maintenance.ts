70f53e82b260909dfdd956290af9a6f9b28e30b0
Jiya <jiyasingla26@gmail.com>
2026-08-13 05:24:17 -0700

    test: revert from shutdown mode

--- apps/web/src/maintenance.ts
+++ apps/web/src/maintenance.ts
@@ -1,6 +1,6 @@
 // Maintenance switch for web
 // Remember to also toggle API maintenance variable
-export const MAINTENANCE_MODE = true;
+export const MAINTENANCE_MODE = false;
 
 //Static page
 export const MAINTENANCE_PAGE = "/maintenance.html";
